c9958cbd7b2c99be163205c3e794a0462524b1e9
Sagicary <sagicary@gmail.com>
2017-07-07 23:00:29 -0400

    Mostrando todos, problema con solicitudes al api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,49 @@
-$(document).ready(function($) {
+$(document).ready(function() {
 	$.ajax({
 		url: 'http://pokeapi.co/api/v2/pokemon',
 		type: 'GET',
 		datatype: 'JSON',
 		data: {"limit":'811'},
+
 	})
+
 	.done(function(data){
-		console.log("Datos si está ok"+ data);
-		console.log (data);
+		//console.log("Datos si está ok"+ data);
+		//console.log (data.results);
+		var hola = data.results;
+		hola.forEach(function(e){
+			//console.log (e.name);
+			//console.log (e.url);
+			$.getJSON(e.url, function(data){
+				var pokeSprite = data.sprites.front_default;
+        		//console.log (pokeSprite); //Me muestra link imagen ok
+        		$("#elementos").append(armarTemplate(e.name , pokeSprite));
+			});
+		})
+		
 	})
 	.fail(function(){
 		console.log("Error");
 	})
-	.always(function(){
+	.always(function(data){
 		console.log("Complete");
-	});
+	})
 });
+
+var armarTemplate = function(nombre,url){
+		var t = "<div class='elemento col-lg-3'><img src='" + url + "'/><h3>"+ nombre +"</h3></div>";
+		return t;
+	}
+/*
 //Probando info encontrada en: http://pokeapi-how.appspot.com/page5
 function pokeSubmit(){
     var param = document.getElementById("pokeInput").value;
     console.log(param);
     var pokeURL = "http://pokeapi.co/api/v1/pokemon/" + param;
     console.log(pokeURL);
-    // new URL for 3rd GET request
-    var pokeURL2 = "http://pokeapi.co/api/v2/pokemon/" + param;
+    var pokeURL2 = "http://pokeapi.co/api/v2/pokemon/" + param; //CONTIENE IMAGENES
     console.log(pokeURL2);
-
-    var pokeURL3 ="http://pokeapi.co/api/v2/pokemon-species/" + param;
+    var pokeURL3 ="http://pokeapi.co/api/v2/pokemon-species/" + param; //CONTIENE DETALLES
 
     $.getJSON(pokeURL2, function(data){ //pido del 2 porque tiene las imagenes
         console.log(data);
@@ -47,9 +64,10 @@ function pokeSubmit(){
         var t = "<div class='elemento'><img src='" + pokeSprite + "'/><h2>"+pokeName+"</h2>"
 		document.getElementById("elementos").innerHTML = t; //Muestro datos en html
 
-	    $.getJSON(pokeURL3, function(data4){
-	           console.log(data4.flavor_text_entries[3].flavor_text); //Muestra descripción en español
-	           console.log(data4.genera[2].genus);
+	    $.getJSON(pokeURL3, function(data2){
+	           console.log(data2.flavor_text_entries[3].flavor_text); //Muestra descripción en español
+	           console.log(data2.genera[2].genus); //Muestra categoria
 		}) //Cierra pokeURL3
     }) // Cierra pokeURL2
 };	// Cierra funcion pokeSubmit
+*/
